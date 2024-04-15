60d60c7839bdd59b1bed26460ae814f5b0d4d98c
Tokle89 <fredriktokle@gmail.com>
2024-04-15 17:44:41 +0200

    finished footer, have venues section left on home page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,23 @@
+import { Link } from "react-router-dom";
+import Logo from "../Logo";
 const Footer = () => (
-  <footer className="border-top border-2 border-blue-700 mt-auto">
-    <p>&copy; 2021 Holidaze</p>
+  <footer className="  bg-primary p-5  text-secondary">
+    <div className="max-w-7xl m-auto flex  justify-between items-start">
+      <Logo />
+      <div className="flex space-x-20 ">
+        <div className="space-y-4">
+          <p className="font-bold">Made With</p>
+          <p>Noroff Api</p>
+          <p>React</p>
+          <p>Tailwind Css</p>
+        </div>
+        <div className="space-y-4 flex flex-col">
+          <p className="font-bold"> Follow Us</p>
+          <Link to="https://github.com/Tokle89/holidaze">Github Project</Link>
+          <Link to="https://www.linkedin.com/in/fredrik-tokle-0994a023b/"> Fredrik Tokle</Link>
+        </div>
+      </div>
+    </div>
   </footer>
 );
 export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
+import CustomButton from "../../components/Button";
 import HeroSection from "../../components/HeroSection";
+import { Link } from "react-router-dom";
+
+const RegisterSection = () => {
+  return (
+    <div className="flex text-primary m-auto max-w-7xl justify-between my-14">
+      <div className="flex flex-col items-center  max-w-md">
+        <p className="mb-7 text-xl ">Create your customer account now, and begin your next adventure</p>
+        <Link to="/register">
+          <CustomButton className="text-white bg-tertiary border-tertiary  hover:bg-white hover:text-tertiary">Register</CustomButton>
+        </Link>
+      </div>
+      <p className=" font-bold text-xl ">Or</p>
+      <div className="flex flex-col items-center  max-w-md">
+        <p className="mb-7 text-xl">Become a host today and unlock a world of opportunities</p>
+        <Link to="/host">
+          <CustomButton className="text-white bg-tertiary border-tertiary  hover:bg-white hover:text-tertiary">Sign up</CustomButton>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 const Home = () => {
   return (
     <main>
       <HeroSection />
+      <RegisterSection />
     </main>
   );
 };
