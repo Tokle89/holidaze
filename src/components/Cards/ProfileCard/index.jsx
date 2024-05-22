283492208741902b6c2a5b6b63b369db10a628e7
Tokle89 <fredriktokle@gmail.com>
2024-05-22 18:44:13 +0200

    adjust styling on profileCard for mobile screen

--- src/components/Cards/ProfileCard/index.jsx
+++ src/components/Cards/ProfileCard/index.jsx
@@ -1,12 +1,12 @@
 const ProfileCard = ({ data: { name, avatar, email, venueManager } }) => {
   return (
-    <div className=" max-w-[350px] flex justify-between p-5 gap-10 items-center ">
+    <div className=" max-w-[600px] flex justify-between p-5 gap-2 md:gap-10 items-center ">
       <div>
-        <img src={avatar.url} alt={name} className="rounded-full w-[80px] h-[80px]" />
+        <img src={avatar.url} alt={name} className="rounded-full w-[40px] h-[40px] md:w-[80px] md:h-[80px]" />
       </div>
       <div className="space-y-2 capitalize">
-        <h1>{name}</h1>
-        <p className="text-gray-800">{email}</p>
+        <h1 className="text-xl sm:text-2xl">{name}</h1>
+        <p className="text-gray-800 text-sm sm:text-base">{email}</p>
         {venueManager && <p className="text-gray-800">Venue Manager</p>}
       </div>
     </div>
