a91106dece1e7c3c6e0387b71547bbb6e6ac00dc
Tokle89 <fredriktokle@gmail.com>
2024-05-22 20:14:33 +0200

    tryging to fix bug where select value bottom is hidden on iphone

--- src/components/Cards/DetailedCard/index.jsx
+++ src/components/Cards/DetailedCard/index.jsx
@@ -221,7 +221,7 @@ const DetailedCard = ({ data, setTriggerFetch }) => {
                 <div className="relative h-10 w-full min-w-[200px]">
                   <select
                     value={guests ? guests : 1}
-                    className="peer h-[2.5em] w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 pt-1 font-sans text-base font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                    className="peer h-[2.5em] w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 pt-1 font-sans text-base font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 appearance-none"
                     onChange={(number) => {
                       setGuests(Number(number.target.value));
                     }}
